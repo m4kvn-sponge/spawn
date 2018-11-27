ffa5ab66202092c507bc2222f312114f2f3d8afe
齊藤正宏 <fun.masahiro@gmail.com>
2018-11-28 01:32:03 +0900

    Implement spawn command

---
+++ src/main/java/com/m4kvn/spawn/Main.kt
@@ -0,0 +1,27 @@
+package com.m4kvn.spawn
+
+import com.m4kvn.spawn.command.SpawnCommand
+import com.m4kvn.spawn.di.MainModule
+import org.koin.log.EmptyLogger
+import org.koin.standalone.StandAloneContext.startKoin
+import org.spongepowered.api.Sponge
+import org.spongepowered.api.command.spec.CommandSpec
+import org.spongepowered.api.event.Listener
+import org.spongepowered.api.event.game.state.GameStartedServerEvent
+import org.spongepowered.api.plugin.Plugin
+
+@Plugin(
+    id = "spawn",
+    name = "Spawn"
+)
+class Main {
+
+    @Listener
+    fun onGameStarted(event: GameStartedServerEvent) {
+        startKoin(listOf(MainModule()), logger = EmptyLogger())
+        val command = CommandSpec.builder()
+            .executor(SpawnCommand())
+            .build()
+        Sponge.getCommandManager().register(this, command, "spawn")
+    }
+}
